375e476808c7eaf5df643f8662effe6d804030bd
LuizArtur29 <luizarmartinsro99@gmail.com>
2025-11-19 17:43:50 -0300

    Consulta simples com query methods

--- src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.nelioalves.workshopmongo.resources;
 
 import com.nelioalves.workshopmongo.domain.Post;
+import com.nelioalves.workshopmongo.resources.util.URL;
 import com.nelioalves.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -17,5 +20,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String title) {
+        title = URL.decodeParam(title);
+        List<Post> list = postService.findByTitle(title);
+        return ResponseEntity.ok().body(list);
     }
 }
