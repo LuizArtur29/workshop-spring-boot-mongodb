317af7a94941fb38208ee80d564603447e2b6076
LuizArtur29 <luizarmartinsro99@gmail.com>
2025-11-19 17:54:37 -0300

    Consulta simples com @query

--- src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -21,6 +21,6 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String title) {
-        return postRepository.findByTitleContainingIgnoreCase(title);
+        return postRepository.findByTitle(title);
     }
 }
